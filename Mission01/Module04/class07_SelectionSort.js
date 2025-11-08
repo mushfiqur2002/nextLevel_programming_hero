c93cdd99e25edcd427aec4b8764febd8f08a0155
mushfiqur2002 <mushfiqurnasim@gmail.com>
2025-11-09 02:51:13 +0600

    ....

--- Mission01/Module04/class07_SelectionSort.js
+++ Mission01/Module04/class07_SelectionSort.js
@@ -1,5 +1,17 @@
-
-let array = [90, 30, 20, 40, 50, 10, 80, 60, 70]
-function selectionSort() {
-
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        if (minIndex !== i) {
+            [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+        }
+    }
+    return arr;
 }
+
+let array = [90, 30, 20, 40, 50, 10, 80, 60, 70];
+console.log(selectionSort(array));
