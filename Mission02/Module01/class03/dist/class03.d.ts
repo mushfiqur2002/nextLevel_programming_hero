a90a83dff597bb48cc39426cdbdc2849bf61c8b0
mushfiqur2002 <mushfiqurnasim@gmail.com>
2025-11-09 16:47:36 +0600

    ts

---
+++ Mission02/Module01/class03/dist/class03.d.ts
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=class03.d.ts.map
---
+++ Mission02/Module01/class03/dist/class03.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const text = 'hello';
+console.log(text);
+//# sourceMappingURL=class03.js.map
